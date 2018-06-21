a28dfaea4ee488c28a0d7ade4af14b66dee88727
byn3 <bryanleung92@gmail.com>
2018-06-21 17:16:02 +0000

    Created Files. Initial commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints out a string using write
+* Description: Program that prints out the string s followed by a new line
+* Return: int 0 when done and no errors
+*/
+
+int main(void)
+{
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+        , 59);
+        return (1);
+}
+
