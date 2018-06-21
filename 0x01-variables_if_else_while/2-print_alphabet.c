10cbd8e488140f3e092f503e3709fc3468de3485
byn3 <bryanleung92@gmail.com>
2018-06-21 19:40:42 +0000

    deleted a file added a file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,25 @@
 
 /**
 * main - prints out a a lowercase alphabet
-* Description: Program that uses a for loop and ASCII to print alphabet
+* Description: Program that uses a while loop, if statement,
+* and ASCII to print alphabet
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
+	int n = 97;
 	char c;
 
-	for (c = 'a'; c <= 'z' ; c++)
-		putchar("%c", c);
+	while (n < 123)
+	{
+		if (n)
+		{
+			c = (char) n;
+			putchar(c);
+			n++;
+		}
+	}
+	putchar(10);
 	return (0);
 }
