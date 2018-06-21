0514ecff01bb463fa511fbdc0f36fa3a2dacc782
byn3 <bryanleung92@gmail.com>
2018-06-21 23:12:59 +0000

    got rid of last comma

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - prints out a string using write
-* Description: Program that prints out the string s followed by a new line
+* main - prints out 0-9 with spaces and commas
+* Description: Program that prints out 0-9 using 1 loop and satements inside
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-        , 59);
-        return (1);
-}
+	int x;
+	int y;
 
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 48; y < 58; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (y == 57 && x == 57)
+				break;
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
