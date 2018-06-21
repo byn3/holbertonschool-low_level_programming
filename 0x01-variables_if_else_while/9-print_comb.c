5e930d6dc5598498731b5a43dd7c62e382ea9dd3
byn3 <bryanleung92@gmail.com>
2018-06-21 22:11:18 +0000

    Wrote script that prints 0-9 with spaces and ,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - prints out a string using write
-* Description: Program that prints out the string s followed by a new line
+* main - prints out 0-9 with spaces and commas
+* Description: Program that prints out 0-9 using 1 loop and satements inside
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-        , 59);
-        return (1);
-}
+	int x;
 
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+		putchar(44);
+		putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
