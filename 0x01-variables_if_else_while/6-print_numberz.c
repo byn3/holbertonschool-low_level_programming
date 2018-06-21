5b625798fb2340fa17cdcab2f64c86482af78133
byn3 <bryanleung92@gmail.com>
2018-06-21 21:26:33 +0000

    Used ASCII to print out numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - prints out a string using write
-* Description: Program that prints out the string s followed by a new line
+* main - prints out 0-9 and a newline
+* Description: Program that prints out numbers using ASCII
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-        , 59);
-        return (1);
+	int i;
+	
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
 }
 
