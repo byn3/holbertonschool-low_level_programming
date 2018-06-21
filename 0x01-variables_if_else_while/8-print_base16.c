a3623091f7349050fd4e4d1ee7b4433b3340cf7f
byn3 <bryanleung92@gmail.com>
2018-06-21 22:02:48 +0000

    Styling for Betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - prints out lowercase alphabet in reverse
-* Description: Program that prints out numbers using ASCII and reverse loop
+* main - prints out 0-9 then a-f
+* Description: Program that prints out alphanum hex first 16 letters
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-	char x;
-		
-	for (x = 48; x > 58; x++)
+	int x;
+
+	for (x = 48; x < 58; x++)
 		putchar(x);
-	for (x = 97; x > 103; x++)
+	for (x = 97; x < 103; x++)
 		putchar(x);
 	putchar(10);
 	return (0);
 }
-
