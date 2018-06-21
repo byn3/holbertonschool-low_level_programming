d99581b5d873f0a05f03478347ec84a86f369688
byn3 <bryanleung92@gmail.com>
2018-06-21 18:57:17 +0000

    Used for loop and ascii to print lower case letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
-* main - prints out a string using write
-* Description: Program that prints out the string s followed by a new line
+* main - prints out a a lowercase alphabet
+* Description: Program that uses a for loop and ASCII to print alphabet
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-        , 59);
-        return (1);
-}
+	char c;
 
+	for (c = 'a'; c <= 'z' ; c++)
+		putchar("%c", c);
+	return (0);
+}
