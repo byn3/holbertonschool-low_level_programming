d5ebed9c59d8705946022ed5e14cfb823c03d45b
byn3 <bryanleung92@gmail.com>
2018-06-21 20:09:39 +0000

    edited comment field

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
-* main - prints out a string using write
-* Description: Program that prints out the string s followed by a new line
+* main - prints out a a lowercase alphabet and upeprcase after
+* Description: Program that uses 2 while loop,
+* and ASCII to print alphabet
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-        , 59);
-        return (1);
-}
+	int n = 97;
 
+	while (n < 123)
+		putchar(n++);
+	n = 65;
+	while (n < 91)
+		putchar(n++);
+	putchar(10);
+	return (0);
+}
