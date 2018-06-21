8fc1242056d928bf832b0bf912b18a239b619881
byn3 <bryanleung92@gmail.com>
2018-06-21 21:31:57 +0000

    Added a reverse alphabet script. decrement loop used

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - prints out a string using write
-* Description: Program that prints out the string s followed by a new line
+* main - prints out lowercase alphabet in reverse
+* Description: Program that prints out numbers using ASCII and reverse loop
 * Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-        , 59);
-        return (1);
+	int i;
+
+	for (i = 122; i < 96; i--)
+		putchar(i);
+	putchar(10);
+	return (0);
 }
 
