62624a95478b471ef37378698dc5ba90ad22a3a0
byn3 <bryanleung92@gmail.com>
2018-06-22 00:35:23 +0000

    Added a quad number advance problem that built off the last problem

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints out quad combo numbers
+* Description: duplicates not allowed. This is the typical
+* x, y= x+1, z= y+1 algorithm. 3 loops nested and put the number
+* a = z+1 now and doing more math. char 32 is a space and 44 is a comma,
+* Return: int 0 when done and no errors
+*/
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+	int a;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 48; y < 58; y++)
+		{
+			for (z = x; z < 58; z++)
+			{
+				for (a = y + 1; a < 58; a++)
+				{
+				putchar(x);
+				putchar(y);
+				putchar(32);
+				putchar(z);
+				putchar(a);
+				if (x == 57 && y == 56 && z == 57 && a == 57)
+					break;
+				putchar(44);
+				putchar(32);
+				}		
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
