1385ad99a2e43e263dcf13095bea9e3c4d0a61a0
byn3 <bryanleung92@gmail.com>
2018-06-22 00:19:06 +0000

    added spacing

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,7 +23,7 @@ int main(void)
 			putchar(x);
 			putchar(y);
 			putchar(z);
-			if (x == 55 && y == 56 && z== 57)
+			if (x == 55 && y == 56 && z == 57)
 				break;
 			putchar(44);
 			putchar(32);
