19442eafb224e97929b7321546f883b4cda7131a
byn3 <bryanleung92@gmail.com>
2018-06-25 23:04:41 +0000

    modiefied the parameter fields

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,5 +4,5 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+int _abs(int x);
+int print_last_digit(int x);
