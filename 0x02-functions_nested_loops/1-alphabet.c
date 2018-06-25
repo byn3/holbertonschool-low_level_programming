45e277dc4bb6d9c0d51e707765caf3c1b25d7cfe
byn3 <bryanleung92@gmail.com>
2018-06-25 21:59:42 +0000

    fixed comments

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* main - program that prints out the alphabet with a new line
+* print_alphabet - program that prints out the alphabet with a new line
 * Description: Uses headers to link to another function
 * Return: int 0 when done and no errors
 */
