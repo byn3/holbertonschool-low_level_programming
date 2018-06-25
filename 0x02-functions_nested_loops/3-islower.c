f86fa890cf75606dbf256998e2a54ca92ca032dc
byn3 <bryanleung92@gmail.com>
2018-06-25 22:19:25 +0000

    added code to check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _islower - checks if the int c is lowercase
+* Description: Uses headers to link to another function and lowercase checker
+* Return: int 0 when done and no errors
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
