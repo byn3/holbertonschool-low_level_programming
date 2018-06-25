79dac6e000e37f21928344af6dd9ff8bcc261525
byn3 <bryanleung92@gmail.com>
2018-06-25 21:01:47 +0000

    added initial files and commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - program that prints out Holberton with a new line
+* Description: Same as above. 
+* Return: int 0 when done and no errors
+*/
+
+int main(void)
+{
+	puts("Holberton");
+	return (0);
+}
