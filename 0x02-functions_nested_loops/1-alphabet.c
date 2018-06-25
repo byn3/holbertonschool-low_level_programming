58ed02770effca38e33a3cc5721651271d8a5dd8
byn3 <bryanleung92@gmail.com>
2018-06-25 21:25:30 +0000

    Linked header files and made basic loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
-* main - program that prints out Holberton with a new line
-* Description: Same as above. 
+* main - program that prints out the alphabet with a new line
+* Description: Uses headers to link to another function
 * Return: int 0 when done and no errors
 */
 
-int main(void)
+int print_alphabet(void)
 {
-	puts("Holberton");
+	int i;
+
+	i = 0;
+	while (i <= 25)
+	{
+		_putchar(i + 'a');
+		i++;
+	}
+	_putchar(10);
 	return (0);
 }
