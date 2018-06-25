fa030f2eb711983775aa36ee89e7006ffaee45c7
byn3 <bryanleung92@gmail.com>
2018-06-25 23:13:52 +0000

    added files that prints the last digit taking sign into account

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* print_last_digit - prints the last digit
+* @x: an int that takes in a number from main
+*
+* Description: Uses headers to link to another function and checcks stuff
+* Probably could just do an easy bit check for the first bit field
+* Return: int 0 when done and no errors
+*/
+
+int print_last_digit(int x)
+{
+	int i = x % 10;
+
+	if (x > 0)
+	{
+		_putchar(i + '0');
+		return (i);
+	}
+	else if (x < 0)
+	{
+		_putchar(-i + '0');
+		return (-i);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
