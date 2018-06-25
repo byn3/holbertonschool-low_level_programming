3d8b5ce5242c09c8c3d39e69d5b0f88eb6c76b7d
byn3 <bryanleung92@gmail.com>
2018-06-25 22:26:41 +0000

    added is it alpha code

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
-* _islower - checks if the int c is a letter
+* _isalpha - checks if the int c is a letter
 * Description: Uses headers to link to another function and letter checker
 * Return: int 0 when done and no errors
 */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c <= 90 && c >= 65))
 		return (1);
