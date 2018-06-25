054bd14fe3d84012133441c0bd881a17b5400e51
byn3 <bryanleung92@gmail.com>
2018-06-25 22:44:09 +0000

    Added the parameter comment

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 
 /**
 * _islower - checks if the int c is lowercase
+* @c: an int passed from the main file
+*
 * Description: Uses headers to link to another function and lowercase checker
 * Return: int 0 when done and no errors
 */
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,8 @@
 
 /**
 * _isalpha - checks if the int c is a letter
+* @c: an int that is passed from the main file
+*
 * Description: Uses headers to link to another function and letter checker
 * Return: int 0 when done and no errors
 */
