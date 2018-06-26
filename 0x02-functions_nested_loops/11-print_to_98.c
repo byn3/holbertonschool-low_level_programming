19162c55b0edbb87554ab38d0ac93887b40f3bab
byn3 <bryanleung92@gmail.com>
2018-06-26 17:05:17 +0000

    added files

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,112 @@
+#include "holberton.h"
+
+/**
+* print_to_98 - function that types n to 98
+* @n: An int given by main
+*
+* Description: Uses headers to link to another function and loops for output
+* Return: int 0 when done and no errors
+*/
+
+void print_to_98(int n)
+{	
+	int i, a, b, benjamin;
+
+	if (n == 98)
+	{
+		_putchar('9');
+		_putchar('8');
+		_putchar('\n');
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if ( i >= 100)
+			{
+				benjamin = i / 100 + '0';
+				a = ((i / 10) % 10) + '0';
+				b = i % 10 + '0';
+				_putchar(benjamin);
+				_putchar(a);
+				_putchar(b);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				a = i / 10 + '0';
+				b = i % 10 + '0';
+				_putchar(a);
+				_putchar(b);
+				if ( i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 0 && i <= -100)/* for -100 and beyond*/
+			{
+				_putchar('-');
+				benjamin = i / 100 + '0';
+				a = ((i / 10) % 10) + '0';
+				b = i % 10 + '0';
+				_putchar(benjamin);
+				_putchar(a);
+				_putchar(b);
+				_putchar(',');
+				_putchar(' ');
+                        }
+			else if (i < 0 && i <= -10)/* for -10 and beyond*/
+			{
+				_putchar('-');
+				a = -i / 10 + '0';
+				b = i % 10 + '0';
+				_putchar(a);
+				_putchar(b);
+				if ( i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else if (i < 0 && i > -10)/* for -9 to -1 */
+			{
+				_putchar('-');
+				b = (-i % 10) + '0';
+				_putchar(b);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ( i >=  0 && i < 10)/* for 0-9 */
+			{
+				b = i % 10 + '0';
+				_putchar(b);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else /* for all 10+. or at least until 98. */
+			{
+				a = i / 10 + '0';
+				b = i % 10 + '0';
+				_putchar(a);
+				_putchar(b);
+				if ( i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+
+			}
+
+		}
+	_putchar('\n');
+	}
+}
