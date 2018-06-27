847fc6c26e0262974e48b649c702e070e1624de8
Bryan <bryanleung92@gmail.com>
2018-06-27 07:15:26 -0700

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,47 +8,44 @@
 
 int main(void)
 {
-	long i = 0, fib1 = 0, fib2 = 1, total = 0;
-	long fib1a, fib1b, fib2a, fib2b, totala, totalb, overflow;
+	int i, overflow;
+	unsigned long fib1 = 1, fib2 = 1, total = 0;
+	unsigned long fib1a, fib1b, fib2a, fib2b, totala, totalb;
 	
+	i = 0;
+	printf("1");
 	while (i <= 98)
 	{
 		if (i < 92)
 		{
 			total = fib1 + fib2;
-			fib2 = fib1;
-			fib1 = total;
-			printf("%ld, ", total);
+			fib1 = fib2;
+			fib2 = total;
+			printf("%lu, ", total);
 		}
-		else if (i == 92)
-		{
-			fib1a = fib1 / 1000000000;
-			fib1b = fib1 % 1000000000;
-			fib2a = fib2 / 1000000000;
-			fib2b = fib2 % 1000000000;
+
+		fib1a = fib1 / 1000000000;
+		fib1b = fib1 % 1000000000;
+		fib2a = fib2 / 1000000000;
+		fib2b = fib2 % 1000000000;
 			
-			overflow = (fib1b + fib2b) / 1000000000;
-			totala = (fib1a + fib2a) + overflow;
-			totalb = (fib1b + fib2b) - overflow * 1000000000;
-			fib1b = fib2b;
-			fib2b = totalb;
-			fib1a = fib2a;
-			fib2a = totala;
-			printf("%ld%ld, ", totala, totalb);
-		}
-		else/* int overflow starts when we at 93*/
+		if (i == 92)
 		{
-			overflow = (fib1b + fib2b) / 1000000000;
-			totala = (fib1a + fib2a) + overflow;
-			totalb = (fib1b + fib2b) - overflow * 1000000000;
-			fib1b = fib2b;
-			fib2b = totalb;
-			fib1a = fib2a;
-			fib2a = totala;
-			if (i < 98)
-				printf("%ld%ld, ", totala, totalb);
-			else
-				printf("%ld%ld", totala, totalb);
+			for (i = 92; i <= 98; i++)/* int overflow starts when we at 93*/
+			{
+				overflow = (fib1b + fib2b) / 1000000000;
+				totala = (fib1a + fib2a) + overflow;
+				totalb = (fib1b + fib2b) - overflow * 1000000000;
+				if (i < 98)
+					printf("%lu%lu, ", totala, totalb);
+				else
+					printf("%lu%lu", totala, totalb);
+				
+				fib1a = fib2a;
+				fib1b = fib2b;
+				fib2a = totala;
+				fib2b = totalb;
+			}
 		}
 		i++;
 	}
