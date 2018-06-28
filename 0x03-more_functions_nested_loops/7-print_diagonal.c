209b6c0134415945b1afb2ffc55e9c941320652b
byn3 <bryanleung92@gmail.com>
2018-06-28 17:47:05 +0000

    added comment to explain code

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,17 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if ( n <= 0)
+	if (n <= 0)/* if 0 or negative just newline */
 		_putchar('\n');
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)/* we want the amount of rows equal to n */
 	{
-		for (j = -i + n; j < n; j++)
+	/*for each column we start at the origin 0,0*/
+	/* when i did j = +i i get a different pattern, the up right pattern*/
+	/* i is the inital spacing needed eg. for 10*/
+	/* j is the counter. j = 10 - 0 , 10 is not less than 10, so skip*/
+	/* now j = 9, 9 < 10 true, do once */
+	/* omg i am a genius */
+		for (j = n - i; j < n; j++)
 		{
 			_putchar(' ');
 		}
