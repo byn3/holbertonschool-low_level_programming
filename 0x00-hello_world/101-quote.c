40c77042e9ebea2b7484ac33eac20dd58ccd2eec
Bryan <bryanleung92@gmail.com>
2018-06-29 12:49:07 -0700

    added comments for future lookers

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,11 @@
 /**
 * main - prints out a string using write
 * Description: Program that prints out the string s followed by a new line
-* Return: int 0 when done and no errors
+* Return: int 0 when done and no errors.
+* Could not use any system calls. Or any standard library functions.
+* Used write tooutput to stderror. Could have tried a method with 2>
+* the bit field is 59. I tried 58, cut off, I tried 60, 61, and higher and
+* it just won't pass because there are prob other things in buffer
 */
 
 int main(void)
