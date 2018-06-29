f6c692be8597a02d0ffea815aa50c53237637b16
Bryan <bryanleung92@gmail.com>
2018-06-29 12:13:47 -0700

    updated with comments

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,6 +6,10 @@
 * Description: Program that combines 2 numbers amd
 * prints the lowest combination of the 2. duplicates like 11 are excluded
 * Return: int 0 when done and no errors
+* A: Using ASCII, range of 0-9.
+* B: The y will be one ahead of x
+* C: If we are at 8 and 9, we break.=, else print the punctuations
+* of a comma(44) and space(32) and a new line at the end(10)
 */
 
 int main(void)
@@ -13,13 +17,13 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 48; x < 58; x++)
+	for (x = 48; x < 58; x++)/* A */
 	{
-		for (y = x + 1; y < 58; y++)
+		for (y = x + 1; y < 58; y++)/* B */
 		{
 			putchar(x);
 			putchar(y);
-			if (x == 56 && y == 57)
+			if (x == 56 && y == 57)/* C */
 				break;
 			putchar(44);
 			putchar(32);
