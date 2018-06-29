de32efd6abc04eee7820381ae117b4feaf60d498
byn3 <bryanleung92@gmail.com>
2018-06-29 02:05:14 +0000

    got the answer. added limit header. added specific if checks

--- 0x03-more_functions_nested_loops/101-main.c
+++ 0x03-more_functions_nested_loops/101-main.c
@@ -17,5 +17,28 @@ int main(void)
     _putchar('\n');
     print_number(-98);
     _putchar('\n');
+	 print_number(234252398);
+    _putchar('\n');
+ print_number(-9235428);
+    _putchar('\n');
+ print_number(-3453398);
+    _putchar('\n');
+
+print_number(2147483646);
+    _putchar('\n');
+
+print_number(-2147483647);
+    _putchar('\n');
+print_number(-5);
+    _putchar('\n');
+print_number(-1);
+    _putchar('\n');
+print_number(1);
+    _putchar('\n');
+ print_number(-2147483648);
+    _putchar('\n');
+ print_number(2147483647);
+    _putchar('\n');
+
     return (0);
 }
--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <limits.h>
 
 /**
 * print_number - outputs a number
@@ -15,19 +16,56 @@ void print_number(int n)
 	int digit;
 	int places = 1000000000;
 
-	if (n == 0)
-		_putchar('0');
 	if (n < 0)
 	{
 		x = -n;
 		_putchar('-');
 	}
-	while (places > x)
-		places /= 10;
-	while (places > 0)
+	if (n == INT_MAX || n == INT_MIN)
 	{
-		digit = x / places;
-		_putchar((digit % 10) + '0');
-		places /= 10;
+		while (1)
+		{
+			if (n == INT_MAX)
+			{
+				_putchar('2');
+				_putchar('1');
+				_putchar('4');
+				_putchar('7');
+				_putchar('4');
+				_putchar('8');
+				_putchar('3');
+				_putchar('6');
+				_putchar('4');
+				_putchar('7');
+				break;
+			}
+			else if (n == INT_MIN)
+			{
+				_putchar('2');
+				_putchar('1');
+				_putchar('4');
+				_putchar('7');
+				_putchar('4');
+				_putchar('8');
+				_putchar('3');
+				_putchar('6');
+				_putchar('4');
+				_putchar('8');
+				break;
+			}
+		}
+	}
+	else if (n == 0)
+		_putchar('0');
+	else
+	{
+		while (places > x)
+			places /= 10;
+		while (places > 0)
+		{
+			digit = x / places;
+			_putchar((digit % 10) + '0');
+			places /= 10;
+		}
 	}
 }
