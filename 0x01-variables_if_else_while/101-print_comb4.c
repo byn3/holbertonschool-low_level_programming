7bef2e6dfa08b0869cafa809f062fbe2e991ccf4
Bryan <bryanleung92@gmail.com>
2018-06-29 12:15:40 -0700

    updated code with comments

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,6 +6,10 @@
 * Description: duplicates not allowed. This is the typical
 * x, y= x+1, z= y+1 algorithm. 3 loops nested and put the number
 * Return: int 0 when done and no errors
+* A: Using ASCII, range of 0-9.
+* B: The y will be one ahead of x, the z is 1 ahead of y.
+* C: If we are at 8 and 9, we break.=, else print the punctuations
+* of a comma(44) and space(32) and a new line at the end(10)
 */
 
 int main(void)
@@ -14,9 +18,9 @@ int main(void)
 	int y;
 	int z;
 
-	for (x = 48; x < 58; x++)
+	for (x = 48; x < 58; x++)/* A */
 	{
-		for (y = x + 1; y < 58; y++)
+		for (y = x + 1; y < 58; y++)/* B */
 		{
 			for (z = y + 1; z < 58; z++)
 			{
@@ -24,7 +28,7 @@ int main(void)
 			putchar(y);
 			putchar(z);
 			if (x == 55 && y == 56 && z == 57)
-				break;
+				break;/* C */
 			putchar(44);
 			putchar(32);
 			}
