1695b34bf995c2edf036f6f4d8cd55c2f5df830c
byn3 <bryanleung92@gmail.com>
2018-06-29 00:57:29 +0000

    got right output. changed code

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -3,67 +3,31 @@
 /**
 * print_number - outputs a number
 * @n: an int given from main
-* 
+*
 * Description: Uses headers to link and a nested loops to achieve goal
 * Return: no return is void
 */
 
+
 void print_number(int n)
 {
 	int x = n;
+	int digit;
+	int places = 1000000000;
 
+	if (n == 0)
+		_putchar('0');
 	if (n < 0)
 	{
 		x = -n;
 		_putchar('-');
 	}
-	while (x > 1000000000)
+	while (places > x)
+		places /= 10;
+	while (places > 0)
 	{
-		x = x / 1000000000;
-		_putchar(x + '0');
+		digit = x / places;
+		_putchar((digit % 10) + '0');
+		places /= 10;
 	}
-	while (x > 100000000)
-	{
-		x = x / 100000000;
-		_putchar(x + '0');
-	}
-	while (x > 10000000)
-	{
-		x = x / 10000000;
-		_putchar(x + '0'); 
-	}
-	while (x > 1000000)
-	{
-		x = x / 1000000;
-		_putchar(x + '0'); 
-	}
-	while (x > 100000)
-	{
-		x = x / 100000;
-		_putchar(x + '0'); 
-	}
-	while (x > 10000)
-	{
-		x = x / 10000;
-		_putchar(x + '0'); 
-	}
-	while (x > 1000)
-	{
-		x = x / 1000;
-		_putchar(x + '0'); 
-	}
-	while (x > 100)
-	{
-		x = x / 100;
-		_putchar(x + '0'); 
-	}
-	while (x > 10)
-	{
-		x = x / 10;
-		_putchar(x + '0'); 
-	}
-	if (n > 0)
-		_putchar((n % 10) + '0');
-	else
-		_putchar ((-n % 10) + '0');
 }
