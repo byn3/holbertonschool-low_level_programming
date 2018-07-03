e5dee010737d802c6f6b566057871c9d2a937388
byn3 <bryanleung92@gmail.com>
2018-07-03 17:43:31 +0000

    added odd numbers test case

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -23,7 +23,7 @@ void puts_half(char *str)
 	odd = counter;
 	if (odd % 2 == 1)
 	{
-		for (i = (counter - 1) / 2; i < counter; i++)
+		for (i = (counter + 1) / 2; i < counter; i++)
 			_putchar(str[i]);
 		_putchar(10);
 	}
