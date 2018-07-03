6aa7d4f9fc898129350111461056db49ba65329d
byn3 <bryanleung92@gmail.com>
2018-07-03 22:11:02 +0000

    added files to attempt last problem

---
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* main - makes something that cracks 101crackme
+* i tried. i have no clue
+*
+* Description: cracking the hacker code
+* Return: returns 0 if no error
+*/
+
+int main(void)
+{
+	return 0;
+}
