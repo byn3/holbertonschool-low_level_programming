9129452df4a1c8c25d9124ad2ed36c4edf7a1607
Bryan <bryanleung92@gmail.com>
2018-07-03 09:17:52 -0700

    added comments and followed betty style

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -7,6 +7,8 @@
 *
 * Description: Uses headers to link and pointers to achieve goal
 * Return: is void. no return.
+* Swaps the values of a and b using pointers and
+* a temp variable that holds one of the values to swap later
 */
 
 void swap_int(int *a, int *b)
