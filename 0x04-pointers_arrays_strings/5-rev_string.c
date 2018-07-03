c0f1114349d166250eb7b2df8bac67787c305a61
Bryan <bryanleung92@gmail.com>
2018-07-03 09:57:19 -0700

    added detailed comments explaining code

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,16 @@
 * Description: Uses headers to link and pointers to achieve goal
 * Return: is void. no return.
 * I tried a sizeof(s) strat at first. testing
+* So to swap and reverse the strings i have
+* a counter variable which will be for the end
+* and an i variable which will start at 0, the beginning,
+* and while counter is greater than i,
+* we store the i index into swap, then we make the i index equal
+* to the counter index which should be the end
+* and we then put swap into counter which should have
+* effectively swapped the two. repeat until we hit the middle
+* because we already swapped the beginning and ends.
+* the middle is the last indices swapped
 */
 
 void rev_string(char *s)
