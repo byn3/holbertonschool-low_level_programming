19c8e673829e5ef93ddfeef831dc92b363c4f878
Bryan <bryanleung92@gmail.com>
2018-07-03 09:20:58 -0700

    added comments and followed betty style

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -6,6 +6,9 @@
 *
 * Description: Uses headers to link and pointers to achieve goal
 * Return: is void. no return.
+* we are guven a string, or a char pointer,
+* we iterate through until the null character
+* we count the number of chars
 */
 
 int _strlen(char *s)
