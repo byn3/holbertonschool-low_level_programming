486ec821489af97ebc78208c94d0c5295ff54c1c
byn3 <bryanleung92@gmail.com>
2018-07-03 20:41:30 +0000

    almost there. need to tweek code a bit onre for certain test cases

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -2,28 +2,35 @@
 
 /**
 * _atoi - convers a string to an int
-* @s 
+* @s: a pointer to a string guven by main
+*
+* Description: making our own atoi function
+* Return: returns 0 if no numbers and the result if numbers
 */
 
 int _atoi(char *s)
 {
 	int counter = 0;
 	int i = 0;
+	int result = 0;
+	int sign = 0;
 
 	while (s[counter] != '\0')
 	{
 		if (s[counter] >= 48 && s[counter] <= 57)
 		{
 			i++;
-			if (s[counter - 1] == '-')
-				_putchar('-');
-			_putchar(s[counter]);
+			if (s[counter] == '-')
+				sign++;
+			result = result * 10 + s[counter] - '0';
 		}
-		if (s[counter] <= 48 || s[counter] >= 57)
-		{
-			if ( i > 0)
-				break;
-		}
+		if (i > 0 && (s[counter] < 48 || s[counter] > 57))
+			break;
 	counter++;
 	}
+	if (i == 0)
+		return (0);
+	if (sign % 2 == 0)
+		return (result);
+	return (result * -1);
 }
