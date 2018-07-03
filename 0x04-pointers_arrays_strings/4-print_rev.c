8d16ee781cbd99a1c8083b7b2dff7682e660ed3c
Bryan <bryanleung92@gmail.com>
2018-07-03 09:52:27 -0700

    added comments and followed betty style

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,12 @@
 *
 * Description: Uses headers to link and pointers to achieve goal
 * Return: is void. no return.
-* I tried a sizeof(s) strat at first. testing
+* I tried a sizeof(s) strat at first.
+* So i have a counter, that will be my _strlen basically.
+* and do a while loop for the amount of my counter,
+* i will put char starting from the end to the beginning
+* but i ran into a problem printing the 0 index
+* so i hardcoded it and out it in at the end before newline
 */
 
 void print_rev(char *s)
