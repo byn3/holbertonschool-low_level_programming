8c18d546a7a7c6c8466edf27740630084ab0c267
Bryan <bryanleung92@gmail.com>
2018-07-03 09:46:35 -0700

    added comments and followed betty style

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -6,6 +6,9 @@
 *
 * Description: Uses headers to link and pointers to achieve goal
 * Return: is void. no return.
+* we are given a character pointer array (string)
+* i measure how long the string is
+* i use the next counter to print the next index
 */
 
 void _puts(char *str)
