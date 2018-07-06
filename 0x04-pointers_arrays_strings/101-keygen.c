f264610793e38c2911f12361be37fbebb3ad9366
byn3 <bryanleung92@gmail.com>
2018-07-06 14:31:20 +0000

    still not correct answer. will fix on weekend

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,8 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 /**
 * main - makes something that cracks 101crackme
@@ -10,5 +14,14 @@
 
 int main(void)
 {
-	return 0;
+	srand((unsigned int)(time(NULL)));
+	int i;
+	char pass[12];
+
+	for(i = 0; i < 12; i++)
+	{
+		pass[i] = 33 + rand() % 94;
+	}
+	pass[i] = '\0';
+	printf("%s\n",pass);
 }
