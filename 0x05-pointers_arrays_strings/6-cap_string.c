cd2f3b9d580fcc4d64bebbb92d02630491feb815
byn3 <bryanleung92@gmail.com>
2018-07-06 19:40:33 +0000

    Files that will capitalize all words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* cap_string- capitalizes all words
+* @s: a pointer that main will give
+*
+* Description: cap all words after a space and period
+* returns the char pointer
+*/
+
+char *cap_string(char *s)
+{
+	while
+}
