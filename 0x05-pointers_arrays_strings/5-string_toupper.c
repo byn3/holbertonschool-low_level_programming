f9831f2e924793ac2d0494f9d8324fa808b93688
byn3 <bryanleung92@gmail.com>
2018-07-06 19:27:10 +0000

    C code to flip all lower to uppercase

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* string_toupper - changes all lowercase to upper
+* @s: a pointer i made that is passed by main
+* Description: changes all lowercase to uppercase
+* Return: returns the pointer;
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+	while (s[i] != 0)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+		i++;
+	}
+	return s;
+}
