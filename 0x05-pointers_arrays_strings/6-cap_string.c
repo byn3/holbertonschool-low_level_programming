2b76e532eb986275b2d515f41dbade9c0cb6eba6
byn3 <bryanleung92@gmail.com>
2018-07-06 20:14:09 +0000

    added all edge cases

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -6,9 +6,38 @@
 *
 * Description: cap all words after a space and period
 * returns the char pointer
+* A: if we have a tab we replace with a space 
+* B: if we have a space or period, caps is true
+* 	or if we have a newline, ;, !, ?, ", (, ), {, and }
+* C: if caps is true and we found a lowercase then upper it
+* D: if caps is true but we dont have a lowercase letter then reset to 0
 */
 
 char *cap_string(char *s)
 {
-	while
+	int i;
+	int caps = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] == '\t')/* A */
+			s[i] = ' ';
+		if (s[i] == '.' || s[i] == ' ' || s[i] == '\n')/* B */
+			caps = 1;
+		else if (s[i] == ',' || s[i] == ';' || s[i] == '!')/* B */
+			caps = 1;
+		else if (s[i] == '?' || s[i] == '"' || s[i] == '(')/* B */
+			caps = 1;
+		else if (s[i] == ')' || s[i] == '{' || s[i] == '}')/* B */
+			caps = 1;
+		else if (caps == 1 && (s[i] <= 97 || s[i] >= 122))/* C */
+			caps = 0;
+		if (caps == 1 && s[i] >= 97 && s[i] <= 122)/* D */
+		{
+			s[i] -= 32;
+			caps = 0;
+		}
+		i++;
+	}
+	return (s);
 }
