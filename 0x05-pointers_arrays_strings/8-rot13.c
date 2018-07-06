a8640d567f85ed86d4c78541672b2bf42d5a921e
byn3 <bryanleung92@gmail.com>
2018-07-06 23:20:27 +0000

    added betty styling to long arrays

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/*
+* rot13 - encodes a string using rot13
+* @s: a pointer given by main
+*
+* Description: change all alpha characters by rot13
+* Return: returns the s pointer
+*/
+
+char *rot13(char *s)
+{
+	int i = 0;
+	int j;
+	int begin[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int end[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O',
+'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
+'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+	while (s[i] != 0)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == begin[j])
+			{
+				s[i] = end[j];
+				break;
+			}
+
+		}
+		i++;
+	}
+	return (s);
+}
