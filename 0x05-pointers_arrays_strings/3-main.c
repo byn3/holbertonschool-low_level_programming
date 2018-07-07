e66c0e38e483b23fa50ca3be43e6f301a488ad34
byn3 <bryanleung92@gmail.com>
2018-07-07 00:20:29 +0000

    changed test cases

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+    char s1[] = "oello";
+    char s2[] = "dorld!";
 
     printf("%d\n", _strcmp(s1, s2));
     printf("%d\n", _strcmp(s2, s1));
