28409c8ed0c085ee20523a21aa9d62a756bbbb12
byn3 <bryanleung92@gmail.com>
2018-07-07 01:41:49 +0000

    initial commit. adding files to try advance problem

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* infinite_add - a function that adds two numbers
+* @n1: a char pointer given by main that represents a num
+* @n2: a char pointer given by main that represents a num
+* @r: a buffer given by main
+* @size_r: the buffer size given by main
+*
+* Description: a function that adds numbers that are passed
+*	as a strings and we add them up
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	
+}
