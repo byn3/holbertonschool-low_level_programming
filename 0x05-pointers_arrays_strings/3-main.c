388fd80876368304d3b6bf2f07f0191961020073
byn3 <bryanleung92@gmail.com>
2018-07-08 19:23:44 +0000

    double checked and added more test cases

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -10,9 +10,15 @@ int main(void)
 {
     char s1[] = "hello";
     char s2[] = "world!";
-
+	
+char s3[] = "hello";
+    char s4[] = "horld!";
+char s5[] = "wello";
+    char s6[] = "aorld!";
     printf("%d\n", _strcmp(s1, s2));
     printf("%d\n", _strcmp(s2, s1));
     printf("%d\n", _strcmp(s1, s1));
+printf("%d\n", _strcmp(s3, s4));
+printf("%d\n", _strcmp(s5, s6));
     return (0);
 }
