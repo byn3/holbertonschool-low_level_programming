78b701032b433c4bceb8dea907ac98623d4eac2c
byn3 <bryanleung92@gmail.com>
2018-07-09 04:12:38 +0000

    ill skip these and come back some other time

---
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* print_buffer - a function that prints a buffer
+* @b: a char pointer given by main that represents a string
+* @size: a size of bytes given by main
+*
+* Description: lots of formatting and stuff to do in this one
+* Return: is void
+*/
+
+void print_buffer(char *b, int size);
+{
+
+}
