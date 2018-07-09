78dc88c45004d11165cbca2a9244f3724d1a8430
byn3 <bryanleung92@gmail.com>
2018-07-09 00:18:43 +0000

    cleaned up code and indentions to make it look nicer

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -6,11 +6,12 @@
 *
 * Description: cap all words after a space and period
 * Return: returns the char pointer
-* A: if we have a tab we caps and not change to a space
-* B: if we have a space or period, caps is true
-*	or if we have a newline, ;, !, ?, ", (, ), {, and }
-* C: if caps is true and we found a lowercase then upper it
-* D: if caps is true but we dont have a lowercase letter then reset to 0
+* if the first char is not uppercase, than uppercase it
+* then we go into a loop until null char
+* we will check to see if we have a white space
+* we then will increase I if white space is found
+* if that car can be capitalized we capitalize it
+* we exit the loop and iterate i. repeat the outer while loop
 */
 
 char *cap_string(char *s)
@@ -21,12 +22,14 @@ char *cap_string(char *s)
 		s[0] -= 32;
 	while (s[i] != 0)
 	{
-		while (s[i] == '\t' || s[i] == '.' || s[i] == ' ' ||
-s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '!' || s[i] == '?'
-|| s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		while (s[i] == '\t' || s[i] == '\n' || s[i] == ' ' ||
+			s[i] == '.' || s[i] == ',' || s[i] == ';' ||
+			s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+			s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+			s[i] == '}')
 		{
 			i++;
-			if (s[i] >= 97 && s[i] <= 122)/* D */
+			if (s[i] >= 97 && s[i] <= 122)
 				s[i] -= 32;
 		}
 		i++;
