0a03ea935cdefa8c07f1cc8303385ff4b41488b9
byn3 <bryanleung92@gmail.com>
2018-07-10 21:19:17 +0000

    similar to script 2 but instead of int return it returns the point address

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -18,10 +18,8 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != 0; i++)
 	{
-		for (j = 0; s[j] != 0; j++)
+		for (j = 0; accept[j] != 0; j++)
 			if (s[i] == accept[j])
-				return (s + i);
-			if (*accept == 0)
 				return (s + i);
 	}
 	return (NULL);
