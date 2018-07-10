d44025fe8ce41d0d1f9e7caef9417f91c96355c7
byn3 <bryanleung92@gmail.com>
2018-07-10 16:39:21 +0000

    added function that copies memory area

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* _memcpy - a function that copies memory area
+* @dest: a char pointer given by main
+* @src: a char source from memory
+* @n: main tells how many bytes of memory the area pointed to
+*
+* Description: Function is very similar to memset from the stan lib
+* Return: returns a pointer to the memory area s
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (n > 0)
+	{
+		s[i] = src;
+		i++;
+		n--;
+	}
+	return (dest);
+}
