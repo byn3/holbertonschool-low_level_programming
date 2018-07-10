4c895f6c1ccb266f0a9d5569709ab006a6478b2c
byn3 <bryanleung92@gmail.com>
2018-07-10 20:22:34 +0000

    added initial files

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* _strpbrk - a function that searches a string for any set of bytes
+* @s: a pointer what will be given by main. returned by s
+* @accept: a string given by main
+*
+* Description: returns a pointer to the byte in s that matches
+*	one of the bytes in accept or null if no such byte
+* Return: a pointer to s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+
+}
