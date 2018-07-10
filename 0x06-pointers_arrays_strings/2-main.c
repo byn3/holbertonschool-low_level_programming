793be70fe880ba4005d0bb39a565fc46d5e722c4
byn3 <bryanleung92@gmail.com>
2018-07-10 17:46:16 +0000

    added more test situations for fun

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -13,9 +13,18 @@ int main(void)
 
     f = _strchr(s, 'l');
     printf("%s\n", f);
+ printf("%p\n", f);
      f = _strchr(s, 'o');
     printf("%s\n", f);
+ printf("%p\n", f);
  f = _strchr(s, 'z');
     printf("%s\n", f);
+ printf("%p\n", f);
+ f = _strchr(s, '\0');
+    printf("%s\n", f);
+ printf("%p\n", f);
+ f = _strchr(s, 'a');
+    printf("%s\n", f);
+
 	return (0);
 }
