d9e983343e9bfdab5e09f71c34429df715978152
byn3 <bryanleung92@gmail.com>
2018-07-10 21:37:53 +0000

    removed a header file

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
 * _strchr- a function that locates a char in a string
@@ -22,5 +21,5 @@ char *_strchr(char *s, char c)
 	}
 	if (c == 0)
 		return (s + i);
-	return (NULL);
+	return (0);
 }
