32812d49f717e48e235d06b0296e445bcdebfa7e
byn3 <bryanleung92@gmail.com>
2018-07-11 17:14:07 +0000

    added calculating factorial code

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* factorial - prints the factorial of a given number
+* @n: an int given by main
+*
+* Description: This will use recursion and no loops
+*	to achieve a goal or mimic a function
+* Return: returns the factorial result
+*/
+
+int factorial(int n)
+{
+	
+}
