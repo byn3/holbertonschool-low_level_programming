db0f3016e2ad56fb31c4cc04eb5d33817fcb406b
byn3 <bryanleung92@gmail.com>
2018-07-11 18:18:06 +0000

    added code to calculate square root

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+* _sqrt_recursion- returns the natural square root of a number
+* @n: an int given by main
+*
+* Description: This will use recursion and no loops
+*	to achieve a goal or mimic a function
+* Return: read first comment line plz
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	
+
+
+
+
+void f1(int n,int i){
+    if((n)-(i*i)<=0)
+        printf("%f",i);
+    else
+        f1(n,i+1);
+}
+
+https://stackoverflow.com/questions/19923953/recursive-algorithm-to-calculate-square-root
+	
+}
