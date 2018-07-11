fb52c7eb6312f79a91b3e8b2449782c963f35337
byn3 <bryanleung92@gmail.com>
2018-07-11 20:20:26 +0000

    added comments

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -2,14 +2,13 @@
 
 /**
 * f1- helper function that takes 2 parameters instead of 1
-* _sqrt_recursion - returns the natural square root of a number
 * @n: the number given from the sq rt function
 * @i: the incremented part. the only part that changes
 *
-* Description: This will use recursion and no loops
-*	to achieve a goal or mimic a function
-* Return: read first comment line plz
+* Description: poop
+* Return: Returns its i value to the sqrt recursion func
 */
+
 
 int f1(int n, int i)
 {
@@ -20,6 +19,13 @@ int f1(int n, int i)
 	return (f1(n, i + 1));
 }
 
+/**
+* _sqrt_recursion- returns only perfect square roots. no decimals
+* @n: the number given from the sq rt function
+*
+* Description: poop
+* Return: Returns its value to the main to print out
+*/
 int _sqrt_recursion(int n)
 {
 	int i = 1;
