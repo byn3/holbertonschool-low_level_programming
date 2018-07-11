d5cfcd0856091028cf070881509afc3b268b76df
byn3 <bryanleung92@gmail.com>
2018-07-11 19:47:31 +0000

    added helper function to handle the logic and parameters

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -9,24 +9,26 @@
 * Return: read first comment line plz
 */
 
+int f1( int n, int i)
+{
+	if (n - (i * i) == 0)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	
+	return f1(n , i + 1);
+}
+
 int _sqrt_recursion(int n)
 {
+	int i = 1;
+
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	
+	return (f1(n, i));
 
-
-
-
-void f1(int n,int i){
-    if((n)-(i*i)<=0)
-        printf("%f",i);
-    else
-        f1(n,i+1);
-}
-
-https://stackoverflow.com/questions/19923953/recursive-algorithm-to-calculate-square-root
+//https://stackoverflow.com/questions/19923953/recursive-algorithm-to-calculate-square-root
 	
 }
