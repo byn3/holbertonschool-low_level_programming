e1923b9e68769d1665a3f57c7eb370e535975963
byn3 <bryanleung92@gmail.com>
2018-07-11 16:08:39 +0000

    changed some variables to pointer variables

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -11,9 +11,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (s)
-	{	_putchar(s);
-		_puts_recursion(&(s + 1));
+	if (*s == 0)
+	{
+		_putchar(10);
+		return;
 	}
-	_putchar(10);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
