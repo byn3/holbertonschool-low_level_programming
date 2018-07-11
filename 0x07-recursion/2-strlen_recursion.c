dfb384271987da15183af567bc5d6e73fa398d4b
byn3 <bryanleung92@gmail.com>
2018-07-11 17:04:47 +0000

    code that returns length of a strng using recursion and no static or global variables

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: a string given by main
+*
+* Description: This will use recursion and no loops
+*	to achieve a goal or mimic a function
+* Return: none it is void.
+*/
+
+int _strlen_recursion(char *s)
+{
+	
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
