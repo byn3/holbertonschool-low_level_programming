b6437182d8c1909584705150e79732e716074abd
byn3 <bryanleung92@gmail.com>
2018-07-13 20:42:19 +0000

    program that adds positive numbers

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: the vector that hold the arguments
+*
+* Description: using arc and argv to achieve the indended function
+* Return: returns 0 if it works or something else if error
+*/
+
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int i = 1;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (argc > 1)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+		argc--;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
