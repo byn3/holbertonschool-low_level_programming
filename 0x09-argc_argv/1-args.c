8ef0125d973bf20b5632f0a8910d6db022d6dc8c
byn3 <bryanleung92@gmail.com>
2018-07-13 19:38:08 +0000

    prints the number of arguments

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - prints its name follwoed by a newline
+* @argc: number of arguments
+* @argv: the vector that hold the arguments
+*
+* Description: prints the current directory and the name like ./name
+* Return: returns the name.
+*/
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
