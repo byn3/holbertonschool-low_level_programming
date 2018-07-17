785501cec1a4373f9aa6924872380e550ce4762f
byn3 <bryanleung92@gmail.com>
2018-07-17 16:37:55 +0000

    changed int size of to char

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = malloc(size * sizeof(int));
+	ptr = malloc(size * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	while (i++ < size)
