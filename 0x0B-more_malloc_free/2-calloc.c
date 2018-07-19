f3a97f74045c218cf0125d5231d0625d4b095633
byn3 <bryanleung92@gmail.com>
2018-07-19 18:11:07 +0000

    followed betty styling

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+* makeitzero - helper function that assigns 0 to all
+* @pointer: the ponter. it will increment its address position
+* @product: the counter basically
+*
+* Description: loops through the void array and makes everyhting 0
+*	the array was void but it was casted and passed in as a char
+* Return: nothing it is void
+*/
+
+void makeitzero(char *pointer, int product)
+{
+	if (product)
+	{
+		*pointer = 0;
+		makeitzero(pointer + 1, product - 1);
+	}
+}
+
+/**
 * _calloc - a function that allocates memory for an array using malloc
 * @nmemb: how many elements
 * @size:the sizeof data type
@@ -12,20 +31,11 @@
 * Return: a pointer or null if errors
 */
 
-void makeitzero(char *pointer, int product)
-{
-	int i = 0;
-
-	for (i = 0; i < product; i++)
-		pointer[i] = 0;
-	return;
-}
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *pointer;
 	int product;
-	
+
 	product = nmemb * size;
 	if (size <= 0 || nmemb <= 0)
 		return (NULL);
