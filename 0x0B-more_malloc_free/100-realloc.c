1a2c4e29079222be1093eeae40f66fb3e0b0f3d1
byn3 <bryanleung92@gmail.com>
2018-07-19 23:18:02 +0000

    removed one line of malloc

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -44,7 +44,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		free(ptr);
-		pointer = malloc(new_size);
+		return (NULL);
 	}
 	else
 		pointer = malloc(new_size);
