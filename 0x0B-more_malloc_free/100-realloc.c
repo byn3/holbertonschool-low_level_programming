5aea2a3a607c725848832fe2ce1054e127f7e4bd
byn3 <bryanleung92@gmail.com>
2018-07-20 00:27:16 +0000

    added a blanket free

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -46,7 +46,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		free(ptr);
 		pointer = malloc(new_size);
 		if (!pointer)
 			return (NULL);
@@ -56,5 +55,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	free(ptr);
 	return (pointer);
 }
