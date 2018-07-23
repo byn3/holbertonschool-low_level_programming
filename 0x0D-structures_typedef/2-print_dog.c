64bfc330bc33076c6b2bd51c619a12aa904e4940
byn3 <bryanleung92@gmail.com>
2018-07-23 19:32:37 +0000

    cahnged nil for float because no null numbers

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -17,8 +17,7 @@ void print_dog(struct dog *d)
 	{
 		(d->name) ? printf("Name: %s\n", d->name) :
 			printf("Name: (nil)\n");
-		(d->age) ? printf("Age: %f\n", d->age) :
-			printf("Age: (nil)\n");
+		printf("Age: %f\n", d->age);
 		(d->owner) ? printf("Owner: %s\n", d->owner) :
 			printf("Owner: (nil)\n");
 	}
