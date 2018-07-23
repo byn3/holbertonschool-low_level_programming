4e54cf3c533ced84dfa08b331342d3a809a3d546
byn3 <bryanleung92@gmail.com>
2018-07-23 19:22:17 +0000

    added edge cases for nulls

--- 0x0D-structures_typedef/2-main.c
+++ 0x0D-structures_typedef/2-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
     struct dog my_dog;
 
-    my_dog.name = "Django";
-    my_dog.age = 3.5;
-    my_dog.owner = "Jay";
+    my_dog.name = 0;
+    my_dog.age = 0;
+    my_dog.owner = 0;
     print_dog(&my_dog);
     return (0);
 }
