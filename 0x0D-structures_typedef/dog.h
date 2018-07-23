26f9da9af598bee0102bbf1f2216a9db6cd60683
byn3 <bryanleung92@gmail.com>
2018-07-23 22:20:15 +0000

    added a prototype

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -28,6 +28,7 @@ typedef struct dog_t
 	float age;
 	char *owner;
 } dog_t;
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
