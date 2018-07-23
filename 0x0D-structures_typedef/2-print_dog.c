b35d4c1b893ccce8070d48e4177101029dc87c66
byn3 <bryanleung92@gmail.com>
2018-07-23 19:23:55 +0000

    removed semicolon

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("\n");;
+		printf("\n");
 	(d->name) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
 	(d->age) ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
 	(d->owner) ? printf("Owner: %s\n", d->owner) :
