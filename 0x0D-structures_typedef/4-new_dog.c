188a748e0996e5443bb2f76f9dea2880e02c5a78
byn3 <bryanleung92@gmail.com>
2018-07-23 20:36:54 +0000

    a function that creates a new doggo

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* new_dog - a pointer that we will return to dog_t
+* @name: woofy mdwoofer
+* @age: wofteen
+* @owner: mr woof
+*
+* Description: makes a new structure that returns a pointer
+* Return: the pointer or null if error
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t *new_doggy;
+
+	new_doggy = malloc(sizeof(struct dog_t));
+	if (!new_doggy)
+		return (NULL);
+	new_doggy->name = name;
+	new_doggy->age = age;
+	new_doggy->owner = owner;
+	return (new_doggy);
+}
