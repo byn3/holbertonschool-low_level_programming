38aeb27555ffc799db807b73dc9d5050b4938137
byn3 <bryanleung92@gmail.com>
2018-07-23 17:27:35 +0000

    Initial commit of first files. Included a readme, and the first problem set where I created the dog struct

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef WOOFWOOF_BOW_WOW_SAYS_LIKE_YOU_AND_SHORTIE_LIKE_MINE
+#define WOOFWOOF_BOW_WOW_SAYS_LIKE_YOU_AND_SHORTIE_LIKE_MINE
+/**
+* struct dog - a structure describing the doggo or pupperino
+* @name: obviously the doge nombre. perro
+* @age: cuanto anos tienen.
+* @owner: dueno de sclavos, dominar.
+*
+* Description: attributes of a doggo
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
