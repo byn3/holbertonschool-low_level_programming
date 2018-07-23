748a81155267c0e5ca01728bcf3693a50760356f
byn3 <bryanleung92@gmail.com>
2018-07-23 18:22:36 +0000

    added edge case for null pointer

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
 * init_dog - a function that initializes a variable of type struct dog
@@ -14,9 +15,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!d)
-		exit(98);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
