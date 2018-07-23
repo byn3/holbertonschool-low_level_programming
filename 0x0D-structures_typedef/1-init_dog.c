4a4b3f9397934ac40641f2d327600e6656477278
byn3 <bryanleung92@gmail.com>
2018-07-23 18:11:23 +0000

    added edge case

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
 * init_dog - a function that initializes a variable of type struct dog
@@ -13,6 +14,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		exit(98);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
