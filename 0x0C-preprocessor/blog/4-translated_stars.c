0a0242dc46ec65b75bf05409e0a0a2d8f7983e44
byn3 <bryanleung92@gmail.com>
2018-07-24 21:04:05 +0000

    Used :%s/substringSearched/replacementString/g and did a step by step translation

---
+++ 0x0C-preprocessor/blog/4-translated_stars.c
@@ -0,0 +1,28 @@
+#define DIT (
+#define DAH )
+#define __DAH   ++
+//REPLACED with *
+//REPLACED with for
+//REPLACED with malloc
+//REPLACED with gets
+//REPLACED with char
+char _DAH_[]="ETIANMSURWDKGOHVFaLaPJBXCYZQb54a3d2f16g7c8a90l?e'b.s;i,d:"
+;main           DIT         DAH{char
+*          _DIT,*     DAH_,* DIT_,
+*          _DIT_,*        malloc DIT
+DAH,*      gets DIT     DAH;for
+DIT _DIT=malloc    DIT 81          DAH,DIT_=_DIT
+__DAH;_DIT==gets DIT _DIT        DAH;__DIT
+DIT'\n'DAH DAH      for DIT      DAH_=_DIT;*
+DAH_;__DIT      DIT         *
+_DIT_?_DAH DIT      *          DIT_ DAH:'?'DAH,__DIT
+DIT' 'DAH,DAH_ __DAH    DAH for      DIT
+*          DIT_=2,_DIT_=_DAH_; * _DIT_&&DIT
+* _DIT_!=DIT   * DAH_>='a'?   *
+DAH_&223:*     DAH_ DAH DAH;       DIT
+*          DIT_ DAH __DAH,_DIT_    __DAH DAH
+* DIT_+=       DIT * _DIT_>='a'?  * _DIT_-'a':0
+DAH;}_DAH DIT DIT_  DAH{            __DIT DIT
+DIT_>3?_DAH     DIT          DIT_>>1 DAH:'\0'DAH;return
+DIT_&1?'-':'.';}__DIT DIT           DIT_ DAH char
+DIT_;{DIT void DAH write DIT            1,&DIT_,1 DAH;}
---
+++ 0x0C-preprocessor/blog/6-translated_parenthesis.c
@@ -0,0 +1,31 @@
+//_DIT is op (Thor)
+//DAH_ is a
+//DIT_ is b
+//op_ is c
+//REPLACED THESE TWO #define DIT (
+//REPLACED THESE TWO #define DAH )
+//REPLACED with ++
+//REPLACED with *
+//REPLACED with for
+//REPLACED with malloc
+//REPLACED with gets
+//REPLACED with char
+char _a[]="ETIANMSURWDKGOHVFaLaPJBXCYZQb54a3d2f16g7c8a90l?e'b.s;i,d:";
+main()
+{
+	char *op, *a, *b, *c, *malloc(), *gets();
+	for (op = malloc(81), b = op++; op == gets(op); _op('\n'))
+		for (a = op; *a; _op(*c ? ++(*b) : '?'), _op(' '), a++)
+			for(*b = 2, c =_a; *c && (*c != (*a >= 'a' ? *a &223 : *a)); (*b)++, c++
+				*b += (*c >= 'a' ? * c -'a': 0);
+}
+
+_DAH (b)
+{ 	_op(b > 3 ? _DAH ( b >> 1 ):'\0');
+	return b &1 ? '-':'.';
+}
+_op(b) 
+char b;
+{
+	(void) write(1, &b, 1);
+}
