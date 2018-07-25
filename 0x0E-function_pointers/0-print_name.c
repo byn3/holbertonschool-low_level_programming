69e71b40765bb595601a765e6c23e0bacae8e90d
byn3 <bryanleung92@gmail.com>
2018-07-25 17:28:26 +0000

    addded the one liner. loll i over thought. its just function callls

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+* print_name - a function that prints a name
+* @name: the name you want printed
+* @f: the function pointer you point to. duh
+*
+* Description: Does stuff. learning function pointers. im hungry
+* Return: void. no return. calls on stuff.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
