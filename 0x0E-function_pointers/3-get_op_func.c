dbc4d80a6d9f3218a1caf1764ad1b7fee7674b43
byn3 <bryanleung92@gmail.com>
2018-07-25 18:22:18 +0000

    added base files for this problem. create my own basic calculator

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,1 @@
+int (*get_op_func(char *s))(int, int);
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,9 @@
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
