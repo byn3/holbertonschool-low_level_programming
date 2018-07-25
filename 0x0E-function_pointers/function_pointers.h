6924fa863cc93ea754bdf2ee62e8481424b18ed5
byn3 <bryanleung92@gmail.com>
2018-07-25 17:54:46 +0000

    added stddef.h

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef NEWHEADERFILENAMEYO
 #define NEWHEADERFILENAMEYO
 
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
