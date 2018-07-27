ee2a950ed6411433fa3d55ac9c91fcf1b3d21d29
byn3 <bryanleung92@gmail.com>
2018-07-27 16:27:48 +0000

    added the 0 edge case

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,8 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i = 0;
 
+	if (n == 0)
+		return (0);
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(list, int);
