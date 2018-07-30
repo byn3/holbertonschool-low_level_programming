d7f07bbc37d31f48e7a8b506f4b3b6f76fec8eb8
byn3 <bryanleung92@gmail.com>
2018-07-30 04:24:51 +0000

    added assembly code for 64 bit that pronts hello world

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,13 @@
 section .data
-    msg db      "hello, world!"
+    msg db      "Hello, Holberton",10
 
 section .text
-    global _start
-_start:
+    global main
+main:
     mov     rax, 1
     mov     rdi, 1
     mov     rsi, msg
-    mov     rdx, 13
+    mov     rdx, 17
     syscall
     mov    rax, 60
     mov    rdi, 0
