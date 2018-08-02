4f9eae783d0f43603f90fe03c81fabffeaa887da
byn3 <bryanleung92@gmail.com>
2018-08-02 17:21:30 +0000

    function that uses recursion to find how many nodes

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* list_len - a function that counts the number of nodes
+* @h: the list given to us from the main
+*
+* Description: prints all the elements of the list_t
+* Return: the number of nodes or -1 if error
+*/
+
+size_t list_len(const list_t *h)
+{
+	if (h->next == NULL)
+		return (1);
+	h = h->next;
+	return (1 + list_len(h));
+}
