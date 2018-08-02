d8717a7632660ecd680205b7d1defa948b6553d7
byn3 <bryanleung92@gmail.com>
2018-08-02 17:20:39 +0000

    added prototype

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -21,9 +21,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/* Prototypes */
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
 
 
 #endif
