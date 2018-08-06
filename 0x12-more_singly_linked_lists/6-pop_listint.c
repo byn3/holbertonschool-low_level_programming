c9bf7d18aed0c07e5595506c24c4c4ec914fa0d4
byn3 <bryanleung92@gmail.com>
2018-08-06 17:48:19 +0000

    function that pops a list and returns the value

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* pop_listint - a function that deletes the head node and return that data
+* @head: the pointer to the pointer head
+*
+* Description: deletes the beginning node
+* Return: the value of the popped node
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	*head = temp->next;
+	free(temp);
+	return (temp->n);
+}
