28149214121a36996f02d898cac90f10490137f7
byn3 <bryanleung92@gmail.com>
2018-08-06 18:24:44 +0000

    function returns the nth node of a linked list

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - a function that returns the nth node of linked list
+* @head: the head node given to us
+* @index: the nth node we want
+*
+* Description: does magic
+* Return: the value of the nth node or null is not exist
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp;
+
+	if (!head)
+		exit(98);
+	temp = head;
+	for (i = 0; i < index && temp != NULL; i++)
+		temp = temp->next;
+	if (i == index)
+		return (temp);
+	return (NULL);
+}
