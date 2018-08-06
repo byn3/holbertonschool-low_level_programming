b6125e2b4a782ce99c95a6f2de6f2f58b1f543dd
byn3 <bryanleung92@gmail.com>
2018-08-06 15:54:43 +0000

    Function that prints a linked lists' integers

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,22 @@
+#include "lists.h"
 
+/**
+* print_listint - a function that prints a linked lists' integers
+* @h: the pointer to the head node given to us
+*
+* Description: kinda like problem 0 from 0x11
+* Return: The number of nodes or -1 if error
+*/
 
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
 
-
-
-size_t print_listint(const listint_t *h);
-
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -1,18 +1,22 @@
 #ifndef MORELINKEDLISTS
 #define MORELINKEDLISTS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for Holberton project
+*/
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 
