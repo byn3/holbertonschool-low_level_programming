d111903689294c6d03f9ab59901bcc0204b4332c
byn3 <bryanleung92@gmail.com>
2018-08-06 16:48:33 +0000

    function that adds a node to the end of the list

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+* add_nodeint_end - adds a node to the end of a linked list
+* @head: the pointer to the head node
+* @n: the int we want copied over
+*
+* Description: Same as 0x11 problem 3
+* Return: the head pointer or error
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *add;
+	listint_t *traverse;
+
+	if (!head)
+		exit(98);
+	traverse = *head;
+	add = malloc(sizeof(listint_t));
+	if (!add)
+		exit(98);
+	add->n = n;
+	add->next = NULL;
+	if (*head == NULL)
+	{
+		*head = add;
+		return (*head);
+	}
+	while (traverse->next)
+	{
+		traverse = traverse->next;
+	}
+	traverse->next = add;
+	return (*head);
+}
