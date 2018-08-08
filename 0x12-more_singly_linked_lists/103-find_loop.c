540945d78cd4737fb27c08a12ba16a4830e1cf41
byn3 <bryanleung92@gmail.com>
2018-08-08 04:37:31 +0000

    problem not finished

---
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+* find_listint_loop - finds where the loop in a linked list is
+* @head: the head node
+*
+* Description: this is wrong i havent gotten it yhet
+* Return: The address of the node where loop starts or null if none
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (!head)
+		return (NULL);
+	return (head);
+}
