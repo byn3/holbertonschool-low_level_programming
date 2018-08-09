ac8938e9425b73ad7d9e1f2db9e5fa4d5dd99a2b
byn3 <bryanleung92@gmail.com>
2018-08-09 15:56:04 +0000

    Initial commit. added base files

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+* binary_to_uint - binary to unsigned int
+* @b: the binary string we are given
+*
+* Description: does stuff via binary and bit manipulation
+* Return: 0 is error or the converted number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int sendback = 1;
+	unsigned int store = 0;
+	int count = 0;
+	int j = 0;
+
+	while (b[i])
+	{
+		if (b[i] < 48 || b[i] > 49 || b == NULL)
+			return (0);
+		i++;
+	}
+	
+	for (count = 0; i >= 0; i--, count++)
+	{
+		sendback = 1;
+		if (b[i] == '1')
+		{
+			for (j = 0; j < count - 1; j++)
+				sendback *= 2;
+			store += sendback;
+		}
+	}
+	return (store);
+} 
---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef BINARY
+#define BINARY
+
+#include <stdio.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
