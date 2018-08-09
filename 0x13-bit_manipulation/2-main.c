47bb72b2bc4c47d5bfd3d46a0e3e636789bf8e7f
byn3 <bryanleung92@gmail.com>
2018-08-09 17:26:58 +0000

    added more test cases

--- 0x13-bit_manipulation/2-main.c
+++ 0x13-bit_manipulation/2-main.c
@@ -16,5 +16,14 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
+
+n = get_bit(0, 0);
+    printf("chould be 0 %d\n", n);
+n = get_bit(1, 0);
+    printf(" should be 1 %d\n", n);
+n = get_bit(1, 1);
+    printf(" should give error or 0 %d\n", n);
+n = get_bit(0, 2);
+    printf("should give error or 0 %d\n", n);
     return (0);
 }
