854dc3ed9f9122dd952e44d07d89daacd24eff1a
byn3 <bryanleung92@gmail.com>
2018-08-09 20:55:41 +0000

    added more test cases

--- 0x13-bit_manipulation/1-main.c
+++ 0x13-bit_manipulation/1-main.c
@@ -8,6 +8,13 @@
  */
 int main(void)
 {
+	print_binary(100);
+    printf("\n");
+print_binary(50);
+    printf("\n");
+print_binary(25);
+    printf("\n");
+
     print_binary(0);
     printf("\n");
     print_binary(1);
