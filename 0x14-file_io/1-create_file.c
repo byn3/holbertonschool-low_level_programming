b6a5d5806b7a652dcf0d87b9beb51e53d2411537
byn3 <bryanleung92@gmail.com>
2018-08-14 21:31:48 +0000

    changed logic of open file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -12,21 +12,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t file;
+	int file;
+	int writing;
 	int count = 0;
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	if (!text_content)
-		text_content = "";
-	for (count = 0; text_content[count]; count++)
-		;
-	if (!text_content)
-		if (write(file, text_content, count) == -1)
-			return (-1);
+		return (1);
+	while (text_content[count])
+		count++;
+	writing = write(file, text_context, count);
+	if (writing == -1)
+		return (-1);
 	close(file);
 	return (1);
 }
