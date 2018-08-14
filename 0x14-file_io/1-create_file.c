0297a28215f666a1e170296a5c9fbf19cb2f24dd
byn3 <bryanleung92@gmail.com>
2018-08-14 21:23:25 +0000

    replaced all ! with NULL

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,16 +15,16 @@ int create_file(const char *filename, char *text_content)
 	ssize_t file;
 	int count = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = "";
 	while (text_content[count])
 		count++;
-	if (!text_content)
+	if (text_content == NULL)
 		if (write(file, text_content, count) == -1)
 			return (-1);
 	close(file);
