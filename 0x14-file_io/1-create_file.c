e2ef429f15da6f1ba9ce7aa9edcf148a036fa687
byn3 <bryanleung92@gmail.com>
2018-08-14 21:25:24 +0000

    changed loop and reverted last commit changes

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,16 +15,16 @@ int create_file(const char *filename, char *text_content)
 	ssize_t file;
 	int count = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
-	while (text_content[count])
-		count++;
-	if (text_content == NULL)
+	for (count = 0; text_content[count]; count++)
+		;
+	if (!text_content)
 		if (write(file, text_content, count) == -1)
 			return (-1);
 	close(file);
