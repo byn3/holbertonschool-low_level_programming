446fa46f19bdc832ef062a8b5e0cec1c86074abb
byn3 <bryanleung92@gmail.com>
2018-08-14 21:47:01 +0000

    removed a = and a *

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 	int reading;
 	int writing;
 	int closing;
-	char *s[1024];
+	char s[1024];
 
 	argument_error97(argc);
 	file = open(argv[1], O_RDONLY);
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
 		writing = write(file2, s, reading);
 		if (writing == -1)
 			write_error99(argv[2]);
-	} while (reading >= 0);
+	} while (reading > 0);
 	closing = close(file);
 	if (closing == -1)
 		close_error100(file);
