5ba98cc7dfa540fabe19b9a785ee86a080850bde
byn3 <bryanleung92@gmail.com>
2018-08-14 17:05:54 +0000

    function that creates a file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* create_file - a function that creates a file
+* @filename: the name pointer
+* @text_content: meh repost.
+*
+* Description: I AM SO SLEEPY.
+* Return: 1 on success or -1 if error
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t file;
+	int count = 0;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	while (text_content[count])
+		count++;
+	if (!text_content)
+		if (write(file, text_content, count) == -1)
+			return (-1);
+	close(file);
+	return (1);
+}
