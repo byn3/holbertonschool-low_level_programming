6d7b7e4f0fb479362c2b39d8d93a742168d9efe3
byn3 <bryanleung92@gmail.com>
2018-10-12 20:40:41 +0000

    betty styling

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -15,26 +15,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL;
 	hash_node_t *check = NULL;
 
-	if (!ht || !key || !strcmp(key, "") || !value)
+	if (!ht || !key || !strcmp(key, ""))
 		return (0); /* if null hash table, key, or empty key, error */
 	index = key_index((unsigned char *)key, ht->size);
-
-	/* check if there a value associated with preexisting key, reassign val */
 	check = ht->array[index];
-	while (check)
+	while (check) /* check if the key already exists, if so replace. */
 	{
 		if (!strcmp(key, value)) /* strcmp returns 0 if matches */
 		{
 			free(check->value); /* get rid of old value, replace it */
 			check->value = strdup(value); /* str dup reallocs */
-			if (!(check->value)) /* if strdup returned an error */
-				return (0);
-			return (1); /* exit cause we updated key's value */
+			if (check->value)
+				return (1);
+			return (0);
 		}
 		check = check->next;
 	}
-
-	/* so there was no matching key, that means new key! */
 	new = malloc(sizeof(hash_node_t)); /* malloc a node */
 	if (!new) /* malloc error */
 		return (0);
@@ -51,9 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->next = NULL; /* everything was assigned with no probs */
-	if (ht->array[index]) /* if there is already a value in this key, */
-		new->next = ht->array[index]; /* make the new node be the first */
-	ht->array[index] = new; /* could just take out the if honestly */
+	new->next = ht->array[index]; /* make the new node be the first */
+	ht->array[index] = new;
 	return (1);
 }
