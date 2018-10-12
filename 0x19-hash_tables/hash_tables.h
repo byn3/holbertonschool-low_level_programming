e95e736d1d03ccc96f00d954f1a51b56293040d7
byn3 <bryanleung92@gmail.com>
2018-10-12 03:46:19 +0000

    added a prototype

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -1,7 +1,8 @@
 #ifndef POTATO
 #define POTATO
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 * struct hash_node_s - Node of a hash table
@@ -35,6 +36,10 @@ typedef struct hash_table_s
 unsigned long int size;
 hash_node_t **array;
 } hash_table_t;
+
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 
 #endif
