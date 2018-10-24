f1cb1863cb6cee074d83e5535c55ea2ab82bbed4
byn3 <bryanleung92@gmail.com>
2018-10-24 20:21:47 +0000

    added prototype

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -1,10 +1,11 @@
 #ifndef SORTED
 #define SORTED
-
 #define _GNU_SOURCE
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
 * struct listint_s - Doubly linked list node
@@ -19,5 +20,12 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+
 
 #endif
