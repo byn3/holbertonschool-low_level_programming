f7b4b2f81a6deffdcd634bef308b21fa8dad28d8
byn3 <bryanleung92@gmail.com>
2018-10-26 23:40:46 +0000

    Change: the logic of the sort method

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -10,40 +10,37 @@
 
 void insertion_sort_list(listint_t **list)
 {
-		size_t i = 1;
-		size_t j;
 		listint_t *head = *list;
-		listint_t *current = head;
-		listint_t *holder = current->prev;
+		listint_t *current;
+		listint_t *holder;
 
-		if (!list || !(*list) || !(list->next))
+		if (!list || !(*list) || !(*list)->next)
 			return;
 		while (head)
 		{
-			head = head->next;
-			while (current && holder && current->n > holder->n)
+			current = head;
+			while (current && current->prev)
 			{
-				swap(holder, current);
-				current = head;
 				holder = current->prev;
-				print_list(*list);
+				if (holder->n > current->n)
+				{
+						(holder->prev)->next = current;
+						current->prev = holder->prev;
+						holder->next = current->next;
+						holder->prev = current;
+						current->next = holder;
+						(holder->next)->prev = holder;
+				}
+				else
+					current = current->next;
 			}
+			head = head->next;
 		}
 }
 
-		/* LOL NOOOO i cant modify the const int n!!!
-		while (temp)
-		{ \* while node exista and the previous value is bigger than current *\/
-			dataStorage = temp->n;
-			temp->n = (temp->prev)->n;
-			(temp->prev)->n = dataStorage;
-			temp = temp->next;
-			
+			/*
 			\* just swap values. do not move any nodes. be super lazy. *\/
-			\* this is just like an array basically. *\/
-		
-		 lol rip my hard wprk im dunb
-  (holder->prev)->next = current;
+				(holder->prev)->next = current;
                 current->prev = holder->prev;
                 holder->next = current->next;
                 holder->prev = current;
