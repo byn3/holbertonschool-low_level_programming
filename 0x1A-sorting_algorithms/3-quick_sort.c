ff91bf547ed8d197de069fe3efb16282c032a121
byn3 <bryanleung92@gmail.com>
2018-10-30 20:56:24 +0000

    Add: base file that has the skeleton of the code format

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+* quick_sort - Uses quick sort to order an array of ints using Lomuto
+* @array: the array of ints we want sorted
+* @size: how big the A is
+*
+* Return: voiderino
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	
+}
