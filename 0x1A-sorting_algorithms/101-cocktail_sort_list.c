79fa060dadab64f26a0db192c2b4c6bf36374ec3
byn3 <bryanleung92@gmail.com>
2018-10-31 20:44:30 +0000

    Add: cocktail sorting algo

---
+++ 0x1A-sorting_algorithms/101-cocktail_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+* cocktail_sort_list - shell sorts
+* @list:  a list. linked.
+*/
+
+void cocktail_sort_list(listint_t **list)
+{
+	if (!list || !(*list) || !(*list)->next)
+		return;
+}
