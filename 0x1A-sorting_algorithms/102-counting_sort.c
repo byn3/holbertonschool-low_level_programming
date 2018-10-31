f4cdbc5a76a1e528884ad4fefa016c30a0f50b4c
byn3 <bryanleung92@gmail.com>
2018-10-31 20:44:53 +0000

    Add: counting sort algo

---
+++ 0x1A-sorting_algorithms/102-counting_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+* counting_sort - sorts
+* @array: array
+* @size: the size of array
+*/
+
+void counting_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
