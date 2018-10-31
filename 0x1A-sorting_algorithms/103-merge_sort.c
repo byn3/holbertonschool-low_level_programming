a13c5327629f0c8c5e005479c06463f5bee58836
byn3 <bryanleung92@gmail.com>
2018-10-31 20:45:04 +0000

    Add: merge sorting algo

---
+++ 0x1A-sorting_algorithms/103-merge_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+* heap_sort - sorts
+* @array: array
+* @size: the size of array
+*/
+
+
+void heap_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
