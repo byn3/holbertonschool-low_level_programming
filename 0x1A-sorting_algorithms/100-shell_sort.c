b7cf7d6aa6d9d42827d1aeae26c15590b0fe8423
byn3 <bryanleung92@gmail.com>
2018-10-31 20:43:50 +0000

    Add: shell sorting algo

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+* shell_sort - shell sorts
+* @array: array
+* @size: the size of array
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
