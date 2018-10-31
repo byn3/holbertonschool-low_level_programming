79f6621319db09b8f9fe59b9e31d928850200e69
byn3 <bryanleung92@gmail.com>
2018-10-31 19:43:00 +0000

    Add: a lot of test cases

--- 0x1A-sorting_algorithms/3-main.c
+++ 0x1A-sorting_algorithms/3-main.c
@@ -9,7 +9,7 @@
 */
 int main(void)
 {
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	int array[] = {1, 1, 1, 1, 1, 1, 1};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
@@ -17,5 +17,31 @@ int main(void)
 	quick_sort(array, n);
 	printf("\n");
 	print_array(array, n);
+
+	printf("________________________________________\n");
+
+	int array1[] = {19, 19, 19, 1};
+
+	n = sizeof(array1) / sizeof(array1[0]);
+
+	print_array(array1, n);
+	printf("\n");
+	quick_sort(array1, n);
+	printf("\n");
+	print_array(array1, n);
+
+	printf("________________________________________\n");
+
+
+	int array2[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+
+	n = sizeof(array2) / sizeof(array2[0]);
+
+	print_array(array2, n);
+	printf("\n");
+	quick_sort(array2, n);
+	printf("\n");
+	print_array(array2, n);
+
 	return (0);
 }
