cc201c456591441edfcfec35778b105464b55e01
byn3 <bryanleung92@gmail.com>
2018-10-31 20:45:50 +0000

    Add: bitonic sorting algo:

---
+++ 0x1A-sorting_algorithms/106-bitonic_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+* bitonic_sort - sorts
+* @array: array
+* @size: the size of array
+*/
+
+
+void bitonic_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
