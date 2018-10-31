2b62740dcee7cc3d947349f2d66798d3b74dc2e4
byn3 <bryanleung92@gmail.com>
2018-10-31 20:36:09 +0000

    Add: all function prototypes in this dir

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -31,6 +31,15 @@ void quick_sort(int *array, size_t size);
 void arraySwap(int *array1, int *array2);
 int lomuto_partition(int *array, int lo, int hi, size_t size);
 void quickie(int *array, int lo, int hi, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+void heap_sort(int *array, size_t size);
 
 
 #endif
