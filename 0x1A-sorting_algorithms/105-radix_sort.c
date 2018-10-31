75249f3d8431bed480dafa52baf7c596e55f3110
byn3 <bryanleung92@gmail.com>
2018-10-31 20:45:35 +0000

    Add: radix sorting algo

---
+++ 0x1A-sorting_algorithms/105-radix_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+* radix_sort - sorts
+* @array: array
+* @size: the size of array
+*/
+
+void radix_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
