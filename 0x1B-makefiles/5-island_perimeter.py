2974c1cdd6d834c636ad341c59efb35db261f182
byn3 <bryanleung92@gmail.com>
2018-11-17 06:46:38 +0000

    added doc stringds to comment code

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-""" algo """
+""" This is a module that finds the
+perimeter of an island, represented by 1s, and
+water is represented by 0s. How many 0s touch the 1s
+in a veritcal or horizontal fashion? """
 
 
 def island_perimeter(grid):
     """ do some funky magic
     WTF WHY DOESNT THIS WORK?!?!
     UGHHHHHH
+
+    Args: Grid is a list of a list of ints
+    Returns: The count
     """
 
     count = 0
@@ -24,4 +30,4 @@ def island_perimeter(grid):
                     count += 1  # left check
                 if grid[i][j + 1] == 0:
                     count += 1  # right check
-    return count
+    return (count)
