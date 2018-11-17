64d83b3feeb49bcbd3122dfa67e092d6e232bf75
byn3 <bryanleung92@gmail.com>
2018-11-17 06:10:22 +0000

    calculates if 1s touch 0s

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" algo """
+
+
+def island_perimeter(grid):
+    """ do some funky magic """
+
+    if grid is None or len(grid) is 0:
+        return 0
+
+    count = 0
+    length = len(grid)
+    length2 = len(grid[0])
+    # print(length, length2)
+
+    for i in range(1, length):
+        for j in range(1, length2):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    count += 1  # up check
+                if grid[i + 1][j] == 0:
+                    count += 1  # down check
+                if grid[i][j - 1] == 0:
+                    count += 1  # left check
+                if grid[i][j + 1] == 0:
+                    count += 1  # right check
+    return count
