601a89960f1968e4afb36733a19b615a1613ed6b
byn3 <bryanleung92@gmail.com>
2018-11-17 06:35:42 +0000

    WHY DOESNT THIS WORK

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -16,12 +16,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0 or i == 0:
+                if grid[i - 1][j] == 0:
                     count += 1  # up check
-                if grid[i + 1][j] == 0 or i == len(grid) - 1:
+                if grid[i + 1][j] == 0:
                     count += 1  # down check
-                if grid[i][j - 1] == 0 or j == 0:
+                if grid[i][j - 1] == 0:
                     count += 1  # left check
-                if grid[i][j + 1] == 0 or j == len(grid[i]) - 1:
+                if grid[i][j + 1] == 0:
                     count += 1  # right check
     return count
