56a21b79e9f5f52592909a4dbc7a588b339a06be
byn3 <bryanleung92@gmail.com>
2018-11-17 06:17:46 +0000

    Change: fixed code to not assume we have a square grid but any shape

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -9,19 +9,19 @@ def island_perimeter(grid):
         return 0
 
     count = 0
-    length = len(grid)
-    length2 = len(grid[0])
+    # length = len(grid)
+    # length2 = len(grid[0])
     # print(length, length2)
 
-    for i in range(length):
-        for j in range(length2):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if grid[i - 1][j] == 0 or i == 0:
                     count += 1  # up check
-                if grid[i + 1][j] == 0 or i == length - 1:
+                if grid[i + 1][j] == 0 or i == len(grid) - 1:
                     count += 1  # down check
                 if grid[i][j - 1] == 0 or j == 0:
                     count += 1  # left check
-                if grid[i][j + 1] == 0 or j == length2 - 1:
+                if grid[i][j + 1] == 0 or j == len(grid[i]) - 1:
                     count += 1  # right check
     return count
