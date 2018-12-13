5e3b611b163c6a54a0fa1fe0cac389383b19c439
byn3 <bryanleung92@gmail.com>
2018-12-13 18:00:51 +0000

    Add: function that prints a new binary node

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - creates a binart tree node
+* @parent: a pointer to the parent
+* @value: the value we want in the new node
+*
+* Return: a pointer to the new node or NULL if fails
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	new->parent = parent;
+	new->n = value;
+	return (new);
+}
