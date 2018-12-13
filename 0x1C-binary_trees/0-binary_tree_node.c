e24c465e4f68676753a776552ab5566073d34873
byn3 <bryanleung92@gmail.com>
2018-12-13 18:19:17 +0000

    Add: malloc fail check

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -13,6 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
+
+	if (!new)
+		return (NULL);
 	new->parent = parent;
 	new->n = value;
 	new->left = NULL;
