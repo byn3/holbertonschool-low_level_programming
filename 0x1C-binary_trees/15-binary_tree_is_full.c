63e7f1190060d274c3d3a996eb3025b24bd00b91
byn3 <bryanleung92@gmail.com>
2018-12-14 21:03:52 +0000

    Change: fixed logic and 0 and 1s

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -10,8 +10,10 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (0);
-	if (tree->left && !(tree->right) || !(tree->left) && tree->right)
 		return (1);
-	return (0 + binary_tree_is_full(tree->left) + binary_tree_is_full(tree->right));
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
