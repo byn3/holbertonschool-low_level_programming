ebfe9ce8f6b812830e72751d63b3ba882ac2b2a3
byn3 <bryanleung92@gmail.com>
2018-12-14 21:39:10 +0000

    Add: code tht finds a pointer to a sibling or null

---
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - checks out the sibling
+* @node: a pointer to the node
+*
+* Return: a tree
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !(node->parent))
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+	
+}
