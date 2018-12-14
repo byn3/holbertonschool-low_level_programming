123432ac4ee26e1bc53a0f471f70b0368740d211
byn3 <bryanleung92@gmail.com>
2018-12-14 21:12:09 +0000

    ChaNGE: 1 to 0

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -10,7 +10,7 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (1);
+		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	if (tree->left && tree->right)
