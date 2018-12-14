a7a856885065298ae6d81f5342c63104059e3ddf
byn3 <bryanleung92@gmail.com>
2018-12-14 21:30:54 +0000

    Add: check to see if the tree is full first

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -2,6 +2,25 @@
 
 
 /**
+* binary_tree_is_full - checks to see if this is a full tree
+* @tree: a pointer to the tree
+*
+* Return: an int
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left)
+			&& binary_tree_is_full(tree->right));
+	return (0);
+}
+
+/**
 * binary_tree_nodes - prints how many nodes with kids
 * @tree: a pointer to the tree
 *
@@ -31,6 +50,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int right = 0;
 
 	if (!tree)
+		return (0);
+	if (!(binary_tree_is_full(tree)))
 		return (0);
 	left = binary_tree_nodes(tree->left);
 	right = binary_tree_nodes(tree->right);
