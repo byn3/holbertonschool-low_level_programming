2a229eaa5c2f517da4ac19967f7f16ea492e64b7
byn3 <bryanleung92@gmail.com>
2018-12-15 01:49:25 +0000

    Add: function that finds the least common ancestor

---
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+*
+*/
+
+binary_tree_t *binary_trees_ancestor(
+			const binary_tree_t *first, const binary_tree_t *second)
+{
+	int i = 0;
+	const binary_tree_t* array[3000];
+
+	if (!first || !second)
+		return (NULL);
+	if (first->parent == NULL || second->parent == NULL)
+		return (NULL);
+
+	while (first)
+	{
+		array[i] = first;
+		first = first->parent;
+		i++;
+	}
+	array[i] = NULL;
+
+	while (second)
+	{
+		i = 0;
+		while (array[i])
+		{
+			if (second == (array[i]))
+				return ((binary_tree_t *) array[i]);
+			i++;
+		}
+		second = second->parent;
+	}
+	return (NULL);
+}
