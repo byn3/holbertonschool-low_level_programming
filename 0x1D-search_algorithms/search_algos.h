dfddfe26ecceae2d8510064d56b197a3d23b65bc
byn3 <bryanleung92@gmail.com>
2019-01-24 16:33:59 +0000

    Add: new function prototype for binary search

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -5,7 +5,8 @@
 #include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
-//int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_helper(int *array, size_t left, size_t right, int value);
 //int jump_search(int *array, size_t size, int value);
 //int interpolation_search(int *array, size_t size, int value);
 //int exponential_search(int *array, size_t size, int value);
